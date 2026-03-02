9e16af9810f0bb59fb8bcbe315e81c3b3460b7be
shoira366 <shoirasaidova366@gmail.com>
2026-03-02 05:30:32 +0500

    Trigger deploy

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('tasks/', views.task_list, name='task_list'),
     path('tasks/create/', views.task_create, name='task_create'),
     path('tasks/<int:pk>/edit/', views.task_update, name='task_update'),
-    path('tasks/<int:pk>/delete/', views.task_delete, name='task_delete'),
+    path('tasks/<int:pk>/delete/', views.task_delete, name='task_delete' ),
 ]
